88492e065e4f1986090a58f82c7df409ba42fe0e
ygidtu <ygidtu@gmail.com>
2019-06-17 11:55:10 +0800

    2019.06.17 fix bugs and increase speed of mean scale.data calculation

--- 7_tmod/cal_scale_mean_by_cell_and_stage.py
+++ 7_tmod/cal_scale_mean_by_cell_and_stage.py
@@ -9,6 +9,7 @@ from glob import glob
 from multiprocessing import Pool
 from subprocess import check_call
 import sys
+import pandas as pd
 
 
 def call(args):
@@ -24,13 +25,19 @@ def call(args):
     
     obj <- readRDS("{0}")
     
-    data <- melt(as.matrix(obj@scale.data))
+    genes = read.table("/mnt/raid62/Lung_cancer_10x/tmod_stage/SYSU/gene.txt")
+
+    data = melt(as.matrix(obj@scale.data[genes[,1],]))
+    
+    # data <- melt(as.matrix(obj@scale.data))
     colnames(data) <- c("gene", "Cells", "value")
     
-    data <- merge(data, obj@meta.data[, c("Cells", "Stage")], by = "Cells")
-    data$Cell = "{1}"
+    data$Stage = obj@meta.data[data$Cells, "Stage"]
+    # data$Cells = "{1}"
     
-    res = as.data.frame(data %>% group_by(Cells, Stage) %>% mutate(m = mean(value)) %>% select(gene, Cells, Stage, m) %>% unique())
+    res = as.data.frame(data %>% group_by(gene, Stage) %>% mutate(m = mean(value)) %>% select(gene, Stage, m) %>% add_tally() %>% unique())
+    
+    res$Cells = "{1}"
     
     write.csv(res, file = "{2}") 
     
@@ -69,10 +76,18 @@ def main():
                     j, cell, os.path.join(output, f"{cell}.csv")
                 ]) 
                 
-    with Pool(10) as p:
+    with Pool(20) as p:
         p.map(call, tasks)
+        
+    data = []
+    for i in tasks:
+        data.append(pd.read_csv(i[2], index_col=0))
+        
+    data = pd.concat(data)
     
-
+    data.to_csv(os.path.join(output, "mean_scale_data.csv"), index=False)
+    
+    
 if __name__ == '__main__':
     main()
         
